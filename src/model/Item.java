8b120ff7f26a1b95477fa183d67e38ac814a3568
Adriana Linda Milena Rodríguez de la Roca <mr.delaroca1@gmail.com>
2026-02-22 21:34:59 -0600

    Constructor Item.

--- src/model/Item.java
+++ src/model/Item.java
@@ -1,5 +1,26 @@
 package model;
 
 public class Item {
-    
+    private OpCode opcode;
+    private byte[] data;
+
+    public Item(OpCode opcode){
+        this.opcode = opcode;
+    }
+
+    public Item(byte[] data){
+        this.data = data;
+    }
+
+    public boolean isOperation(){
+        return opcode != null;
+    }
+
+    public OpCode getOpCode(){
+        return opcode;
+    }
+
+    public byte[] getData(){
+        return data;
+    }
 }
