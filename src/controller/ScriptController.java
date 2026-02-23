2533e8b4fb9db55f5414a06818236043844012cd
JuniorLancerio <juniorlancerio77@gmail.com>
2026-02-22 20:27:10 -0600

    Controller y View terminados.

--- src/controller/ScriptController.java
+++ src/controller/ScriptController.java
@@ -1,5 +1,23 @@
 package controller;
 
+import model.*;
+import view.ScriptView;
+
+import java.util.List;
+
 public class ScriptController {
-    
+
+    private ScriptInterpreter interpreter;
+    private ScriptView view;
+
+    public ScriptController(List<Item> script) {
+        this.interpreter = new ScriptInterpreter(script);
+        this.view = new ScriptView();
+    }
+
+    public void ejecutarScript() {
+        interpreter.setTraceMode(true);
+        boolean resultado = interpreter.execute();
+        view.mostrarResultado(resultado);
+    }
 }
--- src/view/ScriptView.java
+++ src/view/ScriptView.java
@@ -1,5 +1,12 @@
 package view;
 
 public class ScriptView {
-    
+
+    public void mostrarResultado(boolean resultado) {
+        System.out.println("Resultado final: " + resultado);
+    }
+
+    public void mostrarMensaje(String msg) {
+        System.out.println(msg);
+    }
 }
