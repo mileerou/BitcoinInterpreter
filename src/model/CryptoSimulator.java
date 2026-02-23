fbd4c0571bd068df6400250ee0c5d9e144b158a9
Adriana Linda Milena Rodríguez de la Roca <mr.delaroca1@gmail.com>
2026-02-22 21:29:36 -0600

    Clase CryptoSimulator.

--- src/model/CryptoSimulator.java
+++ src/model/CryptoSimulator.java
@@ -1,5 +1,18 @@
 package model;
+import java.util.Arrays;
 
 public class CryptoSimulator {
-    
+    public byte[] hash160(byte[] data){
+        byte[] result = new byte[4];
+
+        for (int i = 0; i < 4 && i < data.lenght; i++){
+            result[i] = data[data.length - 1 - i];
+        }
+
+        return result;
+    }
+
+    public boolean checkSig(byte[] sig, byte[] pubkey){
+        return sig.length == pubkey.length;
+    }
 }
