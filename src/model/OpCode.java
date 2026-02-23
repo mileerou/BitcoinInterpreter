f7722930996e22ec5d8a82d95822a63e62932086
Adriana Linda Milena Rodríguez de la Roca <mr.delaroca1@gmail.com>
2026-02-22 21:38:26 -0600

    Enum OpCode.

--- src/model/OpCode.java
+++ src/model/OpCode.java
@@ -1,5 +1,12 @@
 package model;
 
-public class OpCode {
-    
+public enum OpCode {
+    OP_0,
+    OP_1,
+    OP_DUP,
+    OP_DROP,
+    OP_EQUAL,
+    OP_EQUALVERIFY,
+    OP_HASH160,
+    OP_CHECKSIG
 }
